6ab08b8f51e897dcfce0e25ae0f5fd319d5ac2fb
BhavinKiwiQA <bhavin.nayee@kiwiqa.com>
2020-05-21 12:21:53 +0530

    Few Changes made!!

--- src/Spicejet.java
+++ src/Spicejet.java
@@ -18,6 +18,7 @@ public class Spicejet {
 		
 		Select A = new Select(driver.findElement(By.id("ctl00_mainContent_ddl_Adult")));
 		A.selectByValue("3");
+		A.selectByValue("4");
 		System.out.println(driver.findElement(By.xpath("//div[@id='divpaxinfo']")).getText());
 		driver.close();
 
